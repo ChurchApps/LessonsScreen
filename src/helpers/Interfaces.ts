44fcf32e6513a13bd5400b9d711507e2733fe2e8
Jeremy Zongker <jeremy@trilitech.com>
2022-12-01 21:30:44 -0600

    Updated church interface

--- src/helpers/Interfaces.ts
+++ src/helpers/Interfaces.ts
@@ -3,7 +3,7 @@ export type ApiListType = "MembershipApi" | "AttendanceApi" | "B1Api" | "GivingA
 
 export interface ApiInterface { name: string, keyName?: string, permissions: RolePermissionInterface[], jwt: string }
 export interface ChurchAppInterface { id?: string, churchId?: string, appName?: string }
-export interface ChurchInterface { id?: string, name?: string, registrationDate?: Date, apis?: ApiInterface[], address1?: string, address2?: string, city?: string, state?: string, zip?: string, country?: string, subDomain?: string, personId?: string, jwt?: string, settings?: GenericSettingInterface[] }
+export interface ChurchInterface { id?: string, name?: string, registrationDate?: Date, apis?: ApiInterface[], address1?: string, address2?: string, city?: string, state?: string, zip?: string, country?: string, subDomain?: string, settings?: GenericSettingInterface[] }
 export interface GenericSettingInterface { id?: string, churchId?: string, keyName?: string, value?: string, public?: number }
 export interface IPermission { api: string, contentType: string, action: string }
 export interface RolePermissionInterface { id?: string, churchId?: string, roleId?: string, apiName?: string, contentType?: string, contentId?: string, action?: string }
