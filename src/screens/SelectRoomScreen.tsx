5732403a59a3dd31637e7106844c26cb9fbca69f
Jeremy Zongker <jeremy@zongker.net>
2023-10-13 12:52:04 -0500

    Package update (#7)
    * packages updated
    
    * upgraded few more packages
    
    * Updated typescript
    
    * Changes for the package issues
    
    ---------
    
    Co-authored-by: Payal <payalpie0@gmail.com>

--- src/screens/SelectRoomScreen.tsx
+++ src/screens/SelectRoomScreen.tsx
@@ -1,6 +1,6 @@
 //import AsyncStorage from '@react-native-community/async-storage';
 import React, { useEffect } from 'react'
-import { Image, View, Text, FlatList, TouchableHighlight, ListRenderItem, ActivityIndicator, BackHandler } from 'react-native'
+import {  View, Text, FlatList, TouchableHighlight, ListRenderItem, ActivityIndicator, BackHandler } from 'react-native'
 import { ApiHelper, CachedData, ClassroomInterface, Styles, Utilities } from "../helpers";
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from "../helpers/CustomReactNativeResponsiveScreen";
 import { MenuHeader } from '../components';
