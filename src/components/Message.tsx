6fb2954697940cfd4cfef0d7ba7bafc8d0caca09
Payal P <127848097+Payalpie@users.noreply.github.com>
2024-09-25 20:08:17 +0530

    Lessons.Church issue fix #281 (#10)

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -14,39 +14,54 @@ type Props = {
 
 export const Message = (props: Props) => {
 
-  const getMessageType = () => {
+  const [internalPaused, setInternalPaused] = React.useState(props.paused);
+
+  React.useEffect(() => {
+    setInternalPaused(props.paused);
+  }, [props.paused]);
+
+  // const getMessageType = () => {
+  //   const parts = props.file.url.split("?")[0].split(".");
+  //   const ext = parts[parts.length - 1];
+  //   let result = "image"
+  //   switch (ext.toLocaleLowerCase()) {
+  //     case "webm":
+  //     case "mp4":
+  //       result = "video"
+  //       break;
+  //   }
+
+  //   if (props.file.url.indexOf("externalVideos") > -1) result = "video";
+
+  //   //console.log("Message Type:", result, props.file.url.split("?")[0])
+  //   return result;
+  // }
+
+  const getMessageType = (): 'image' | 'video' => {
     const parts = props.file.url.split("?")[0].split(".");
-    const ext = parts[parts.length - 1];
-    let result = "image"
-    switch (ext.toLocaleLowerCase()) {
-      case "webm":
-      case "mp4":
-        result = "video"
-        break;
+    const ext = parts[parts.length - 1].toLowerCase();
+    if (ext === "webm" || ext === "mp4" || props.file.url.indexOf("externalVideos") > -1) {
+      return "video";
     }
+    return "image";
+  };
 
-    if (props.file.url.indexOf("externalVideos") > -1) result = "video";
-
-    //console.log("Message Type:", result, props.file.url.split("?")[0])
-    return result;
-  }
-
-  const getContent = () => {
-    let result = <></>
-    switch (getMessageType()) {
-      case "image":
-        result = getImage();
-        break;
-      case "video":
-        result = getVideo();
-        break;
-    }
-    return result
-  }
+  // const getContent = () => {
+  //   let result = <></>
+  //   switch (getMessageType()) {
+  //     case "image":
+  //       result = getImage();
+  //       break;
+  //     case "video":
+  //       result = getVideo();
+  //       break;
+  //   }
+  //   return result
+  // }
 
   const getVideo = () => {
     const filePath = (props.downloaded) ? "file://" + CachedData.getFilePath(props.file.url) : props.file.url;
-    return (<Video source={{ uri: filePath }} repeat={props.file.loopVideo} resizeMode="cover" style={{ width: DimensionHelper.wp("100%"), height: DimensionHelper.hp("100%") }} poster={(props.downloaded) ? "" : "https://lessons.church/images/loading.png"} paused={props.paused} />)
+    return (<Video source={{ uri: filePath }} repeat={props.file.loopVideo} resizeMode="cover" style={{ width: DimensionHelper.wp("100%"), height: DimensionHelper.hp("100%") }} poster={(props.downloaded) ? "" : "https://lessons.church/images/loading.png"} paused={internalPaused} />)
   }
 
   const getImage = () => {
@@ -54,9 +69,16 @@ export const Message = (props: Props) => {
     return (<Image source={{ uri: filePath }} style={{ width: DimensionHelper.wp("100%"), height: DimensionHelper.hp("100%") }} />);
   }
 
-  return getContent()
+  const content = React.useMemo(() => {
+    return getMessageType() === "video" ? getVideo() : getImage();
+  }, [props.file, internalPaused, props.downloaded]);
+
+  return content;
+
+  // return getContent()
 
 }
+
 
 
 
