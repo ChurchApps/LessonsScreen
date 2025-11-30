6a4b5912800bd9908ccae529ddee343b66eb8d5b
Jeremy Zongker <jeremy@zongker.net>
2025-11-29 20:17:02 -0600

    Added eas update check

--- App.tsx
+++ App.tsx
@@ -4,18 +4,30 @@ import { Navigator } from "./src/navigation/Navigator";
 import { EnvironmentHelper } from "./src/helpers/EnvironmentHelper";
 import { LogBox } from "react-native";
 import { ErrorHelper } from "@churchapps/mobilehelper";
-
-
+import * as Updates from "expo-updates";
 
 EnvironmentHelper.init();
+
+const checkForUpdates = async () => {
+  if (__DEV__) return;
+  try {
+    const update = await Updates.checkForUpdateAsync();
+    if (update.isAvailable) {
+      await Updates.fetchUpdateAsync();
+      await Updates.reloadAsync();
+    }
+  } catch (error) {
+    console.log("Error checking for updates:", error);
+  }
+};
 
 const App = () => {
   LogBox.ignoreLogs(['new NativeEventEmitter']);
 
-
-
-  
-  useEffect(() => { ErrorHelper.init(); }, []);
+  useEffect(() => {
+    ErrorHelper.init();
+    checkForUpdates();
+  }, []);
 
   return <Navigator />
 
